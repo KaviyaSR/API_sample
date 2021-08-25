ca0172dc033457978f5b4d8f395d9b5aa0c23ca4
Kaviya <kaviya12198@gmail.com>
2021-08-26 03:01:17 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App=() => {
+
+const [pokemon, setPokemon]= useState("pikachu");
+const [pokemonData, setPokemonData]= useState([]);
+const [pokemonType, setPokemonType]= useState("");
+
+const getPokemon= async ()=> {
+  const toArray=[];
+try{
+ const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+ const res = await axios.get(url);
+ toArray.push(res.data);
+ setPokemonType(res.data.types[0].type.name);
+ setPokemonData(toArray);
+ console.log(res); 
+}
+catch(e){
+console.log(e);
+}
+}
+
+const handleChange=(e)=> {
+  setPokemon(e.target.value.toLowerCase());
+
+}
+const handleSubmit =(e)=>{
+  e.preventDefault();
+  getPokemon();
+}
+
+//useEffect(() => {
+  //getPokemon();
+//}, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input type='text' placeholder='Enter Character Name' onChange={handleChange}>
+          
+          </input>
+        </label>
+      </form>
+      {pokemonData.map((data)=>{
+       return(
+
+    <div className='container'>
+      <img  src={data.sprites["front_default"] }/>
+
+  
+  
+  <div clasName="divTableCell">Type</div>
+  <div clasName="divTableCell">{pokemonType}</div>
+  
+ 
+  <div clasName="divTableCell">Height</div>
+  <div clasName="divTableCell">{""} {Math.round(data.height* 3.9)}</div>
+  
+  
+  <div clasName="divTableCell">Weight</div>
+  <div clasName="divTableCell">{""} {Math.round(data.weight/ 4.9)}lbs</div>
+  
+  
+</div>
+
+     
+       )
+      })}
+      
     </div>
   );
+  
 }
 
 export default App;
